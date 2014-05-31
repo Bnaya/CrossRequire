6a8299c98252d2f139fbc49e7be22bc3f4d4fe4d
Bnaya Peretz <bnaya@photomania.net>
2014-06-01 01:05:46 +0300

    fix issue #5

--- extension.js
+++ extension.js
@@ -1,6 +1,13 @@
 // We need to eval the require.js code on the topmost scope we've got.
 // If it will start making problems we can try indirect eval call
 // see: http://perfectionkills.com/global-eval-what-are-the-options/
+
+// prevent our requirejs instance to try and load requirejs main file on the page
+// Fixes issue #5
+var require = {
+    skipDataMain: true
+};
+
 eval(appAPI.resources.get('vendor/requirejs/require.js'));
 
 appAPI.ready(function () {
